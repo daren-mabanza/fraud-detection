109bb2c0db58ada2800eb4a20a394717eda7452b
Daren Mabanza <daren77186@icloud.com>
2026-02-19 20:59:09 +0100

    post eda script

---
+++ 03_fonctions/cleaning/post_eda.py
@@ -0,0 +1,50 @@
+# ======================
+from pathlib import Path
+import pandas as pd
+import numpy as np
+# ======================
+
+ROOT = Path.cwd().parents[0]
+
+RAW_DATA = ROOT / "01_data" / "01_raw"
+PROCESSED_DATA = ROOT / "01_data" / "02_processed"
+
+# ======================
+
+def cleaning_post_eda():
+
+    # Import des données
+    data_fraud = pd.read_parquet(PROCESSED_DATA / "data_fraud_processed.parquet")
+    df = data_fraud.copy()
+
+    print("Import des données : OK")
+    print("="*50)
+
+
+    # Selection des données de 2020
+    df = df[df["annee_transaction"]==2020]
+
+    print("Selection des données de 2020 : OK")
+    print("="*50)
+
+
+    # Nettoyage de la variable 'etat_client'
+    df["etat_client"] = np.where(
+        df["etat_client"].isin(["DC","HI","AK","RI"]),
+        "other",
+        df["etat_client"]
+    )
+
+    print("Nettoyage de la variable 'etat_client' : OK")
+    print("="*50)
+
+
+    # Export de la table modifiée
+    df.to_parquet(PROCESSED_DATA / "data_fraud_eda_processed.parquet")
+
+
+    print("Export de la table modifiée : OK")
+    print("="*50)
+
+
+
