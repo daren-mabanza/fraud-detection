95b71dd32d2637c3e78a627a65eda26d1c39580f
Daren Mabanza <daren77186@icloud.com>
2026-03-22 16:18:30 +0100

    onglet 4 fix

--- 06_streamlit/fonctions_streamlit/onglet_4.py
+++ 06_streamlit/fonctions_streamlit/onglet_4.py
@@ -23,8 +23,9 @@ JOBLIB_DATA = ROOT / "01_data" / "03_joblib"
 def onglet_4():
     st.header("Votre transaction serait-elle considérée comme frauduleuse ?")
     
-    st.info(
+    st.markdown(
     """
+    <div style="font-size:1.15rem;">
     ℹ️ **Avertissement**
 
     Le modèle a été entraîné sur des **transactions simulées**.  
@@ -33,6 +34,7 @@ def onglet_4():
     Une transaction pouvant paraître très étrange dans la réalité ne sera donc pas forcément classée comme très risquée ici, si ce type de cas n’est pas correctement représenté dans les données simulées d’entraînement.
 
     Ce score doit être lu comme une **illustration du fonctionnement du modèle dans un cadre simulé**.
+    </div>
     """
     )
 
