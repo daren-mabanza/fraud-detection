895ffc4032294c2ccc09b239cf6e49b910d7a547
Daren Mabanza <daren77186@icloud.com>
2026-03-26 17:39:35 +0100

    onglet 4 fix

--- 06_streamlit/fonctions_streamlit/onglet_4.py
+++ 06_streamlit/fonctions_streamlit/onglet_4.py
@@ -31,6 +31,23 @@ def onglet_4():
     """,
     unsafe_allow_html=True
 )
+    
+    st.markdown(
+    """
+    <div style="font-size:0.9rem; line-height:1.5; margin-bottom:20px;">
+        <strong>⚠️ Avertissement</strong><br><br>
+
+        Le modèle a été entraîné sur des <strong>transactions simulées</strong>.<br>
+        La probabilité affichée dans cet onglet indique donc comment la transaction créée se positionne par rapport aux 
+        <strong>schémas appris dans ces données générées</strong>, et non par rapport à l’ensemble des situations réelles possibles.<br>
+        Une transaction pouvant paraître très étrange dans la réalité ne sera donc pas forcément classée comme très risquée ici, 
+        si ce type de cas n’est pas correctement représenté dans les données simulées d’entraînement.<br><br>
+
+        Ce score doit être lu comme une <strong>illustration du fonctionnement du modèle dans un cadre simulé</strong>.
+    </div>
+    """,
+    unsafe_allow_html=True
+        )
 
     # États qu'on propose explicitement dans l'UI
     etats_principaux = ["CA", "NY", "TX", "FL", "NJ"]
@@ -203,19 +220,4 @@ def onglet_4():
         }
         st.table(pd.DataFrame(recap))
         
-        st.markdown(
-    """
-    <div style="font-size:1rem; line-height:1.5; margin-bottom:20px;">
-        <strong>⚠️ Avertissement</strong><br><br>
-
-        Le modèle a été entraîné sur des <strong>transactions simulées</strong>.<br>
-        La probabilité affichée dans cet onglet indique donc comment la transaction créée se positionne par rapport aux 
-        <strong>schémas appris dans ces données générées</strong>, et non par rapport à l’ensemble des situations réelles possibles.<br>
-        Une transaction pouvant paraître très étrange dans la réalité ne sera donc pas forcément classée comme très risquée ici, 
-        si ce type de cas n’est pas correctement représenté dans les données simulées d’entraînement.<br><br>
-
-        Ce score doit être lu comme une <strong>illustration du fonctionnement du modèle dans un cadre simulé</strong>.
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+        
