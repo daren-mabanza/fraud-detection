f9b67c66696f8f341f18b48986357ae6e9bad68b
Daren Mabanza <daren77186@icloud.com>
2026-03-26 17:42:35 +0100

    onglet4 fix

--- 06_streamlit/fonctions_streamlit/onglet_4.py
+++ 06_streamlit/fonctions_streamlit/onglet_4.py
@@ -35,13 +35,11 @@ def onglet_4():
     st.markdown(
 """<div style="font-size:0.9rem; line-height:1.5; margin-bottom:20px;">
 <strong>⚠️ Avertissement</strong><br><br>
-
 Le modèle a été entraîné sur des <strong>transactions simulées</strong>.<br>
 La probabilité affichée dans cet onglet indique donc comment la transaction créée se positionne par rapport aux 
-<strong>schémas appris dans ces données générées</strong>, et non par rapport à l’ensemble des situations réelles possibles.<br>
+<strong>schémas appris dans ces données générées</strong>, et non par rapport à l’ensemble des situations réelles possibles.
 Une transaction pouvant paraître très étrange dans la réalité ne sera donc pas forcément classée comme très risquée ici, 
-si ce type de cas n’est pas correctement représenté dans les données simulées d’entraînement.<br><br>
-
+si ce type de cas n’est pas correctement représenté dans les données simulées d’entraînement.
 Ce score doit être lu comme une <strong>illustration du fonctionnement du modèle dans un cadre simulé</strong>.
 </div>""",
 unsafe_allow_html=True
